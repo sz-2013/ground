046a399067476dcbcd5f131439f00a68521b52b2
beebikun <beebikun@gmail.com>
2014-09-25 16:06:24 +1000

    add parallax

--- ground/views.py
+++ ground/views.py
@@ -4,8 +4,8 @@ from ground.models import Image
 
 
 def index(request):
-    # img = Image.objects.order_by('?')[0]
-    img = Image.objects.get(name="futuri-gate")
+    img = Image.objects.order_by('?')[0]
+    # img = Image.objects.get(name="futuri-gate")
     data = dict(data=img)
     context = RequestContext(request, data)
     template = loader.get_template('index.html')
